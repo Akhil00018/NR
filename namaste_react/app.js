bc40b7a84be23e7565b03c78e31967b40e73cc24
Akhil00018 <166279360+Akhil00018@users.noreply.github.com>
2025-11-05 10:45:45 +0530

    Add files via upload

---
+++ namaste_react/app.js
@@ -0,0 +1,8 @@
+const parent=React.createElement("div",{id:"parent"},
+    React.createElement("div",{id:"child"},
+        [React.createElement("h1",{},"hi hello "),
+        React.createElement("h2",{},"hi hello iam green ")]));
+
+        const root=ReactDOM.createRoot(document.getElementById("root"));
+
+        root.render(parent);
